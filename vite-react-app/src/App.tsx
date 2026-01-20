9e1ca37dd1ea76d130239ca42fcbb9212c5039fe
Asilbek2706 <asilbekkaromatov2@gmail.com>
2026-01-20 21:00:12 +0500

    Update

--- vite-react-app/src/App.tsx
+++ vite-react-app/src/App.tsx
@@ -8,28 +8,7 @@ function App() {
         skills: ["ReactJS", "Typescript", "Vite"],
         avatar: "https://api.dicebear.com/7.x/avataaars/svg?seed=Felix"
     };
-
-
-    const product = {
-        name: "iPhone 15 Pro",
-        price: 999,
-        discountPrice: 899, //chegirma bor
-        category: "Smartfonlar",
-        stock: 5, //qoldiq
-        features: ["A17 Pro chip", "Titanium korpus", "48MP Camera"],
-        image: "https://v-p.com.ua/wp-content/uploads/2023/09/iphone-15-pro-max-finish-select-202309-6-7-inch-black-titanium.webp",
-        isAvailable: true
-    };
-
-    const styleObject = {
-        border: "1px solid #ddd",
-        borderRadius: "12px",
-        padding: "20px",
-        maxWidth: "350px",
-        boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
-        textAlign: "center"
-    };
-
+    
 //        inline style
     const cardStyle = {
         border: "2px solid #646cff",
@@ -63,50 +42,10 @@ function App() {
                     <hr />
 
                     <h3>Ko'nikmalar:</h3>
-                    <p>{user.skills.join(" * ")}</p>
+                    <p>{user.skills.join(" . ")}</p>
 
                     <small>Bugun: {new Date().toLocaleDateString()}</small>
                 </div>
-            </div>
-
-            <div style={ styleObject }>
-                <h4 style={{ color: "gray", textTransform: "uppercase", fontSize: "12px" }}>{product.category}</h4>
-                <img src={product.image} alt={product.name} style={{ width: '150px', borderRadius: "8px" }} />
-
-                <h2>{product.name}</h2>
-
-                <div>
-                    <span style={{ textDecoration: "line-through", color: "gray", marginRight: "10px" }}>
-                        ${product.price}
-                    </span>
-                    <span style={{ color: "red", fontWeight: "bold", fontSize: "20px" }}>
-                        ${product.discountPrice}
-                    </span>
-                </div>
-
-                <div style={{ textAlign: "left", margin: "15px 0" }}>
-                    <strong>Xususiyatlari:</strong>
-                    <ul>
-                        {product.features.map((feature, index) => (
-                            <li key={index}>{feature}</li>
-                        ))}
-                    </ul>
-                </div>
-
-                <p style={{ color: product.stock > 0 ? "green" : "red", fontWeight: "bold" }}>
-                    {product.stock > 0 ? `Sotuvda bor: ${product.stock} ta` : "Sotuvda yo'q"}
-                </p>
-
-                <button disabled={!product.isAvailable} style={{
-                    backgroundColor: product.isAvailable ? "#007bff" : "#ccc",
-                    color: "white",
-                    padding: "10px 20px",
-                    border: "none",
-                    borderRadius: "5px",
-                    cursor: product.isAvailable ? "pointer" : "not-allowed"
-                }}>
-                    {product.isAvailable ? "Sotib olish" : "Mavjud emas"}
-                </button>
             </div>
         </>
     );
