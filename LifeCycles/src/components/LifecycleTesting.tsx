9d19e3a756f74ec0217da31984e8f74f1978a09d
Asilbek2706 <asilbekkaromatov2@gmail.com>
2026-01-23 18:30:40 +0500

    Add some examples

--- LifeCycles/src/components/LifecycleTesting.tsx
+++ LifeCycles/src/components/LifecycleTesting.tsx
@@ -45,6 +45,25 @@ const LifecycleTesting = () => {
         }
     }, []);
 
+    const [text, setText] = useState("");
+
+    useEffect(() => {
+        if (text.length > 0) {
+            document.title = `Yozilmoqda: ${text}`;
+        } else {
+            document.title = "React App";
+        }
+
+        console.log("tab sarlavhasi o'zgardi!")
+    }, [text]);
+
+    useEffect(() => {
+        return () => {
+            document.title = "React App";
+            console.log("document o'chdi sarlavha tiklandi!")
+        }
+    }, []);
+
     const handleIncrement= () => {
         setCount(count => count + 1);
     }
@@ -61,29 +80,35 @@ const LifecycleTesting = () => {
     return <div>
         <button onClick={handleIncrement}>Increment</button>
         <h1>Count: {count}</h1>
+
+        <hr />
+
+        <div style={{ padding: '20px', border: '1px solid #ccc' }}>
+            <p>Tepadagi brawser tabiga qarang!</p>
+        </div>
     </div>
 }
 
-const WindowWidthTracker = () => {
-    const [width, setWidth] = useState(window.innerWidth);
+// const WindowWidthTracker = () => {
+//     const [width, setWidth] = useState(window.innerWidth);
+//
+//     useEffect(() => {
+//         const handleResize = () => setWidth(window.innerWidth);
+//
+//
+//         // Mounting
+//         window.addEventListener('resize', handleResize)
+//         console.log("Oynani kuzatish boshlandi!")
+//
+//         //Unmounting
+//         return () => {
+//             window.removeEventListener('resize', handleResize)
+//             console.log("Oynani kuzatish to\'xtatildi!")
+//         }
+//     }, []);
+//
+//     return <div>Oyna kengligi: {width}px</div>
+// }
 
-    useEffect(() => {
-        const handleResize = () => setWidth(window.innerWidth);
-
-
-        // Mounting
-        window.addEventListener('resize', handleResize)
-        console.log("Oynani kuzatish boshlandi!")
-
-        //Unmounting
-        return () => {
-            window.removeEventListener('resize', handleResize)
-            console.log("Oynani kuzatish to\'xtatildi!")
-        }
-    }, []);
-
-    return <div>Oyna kengligi: {width}px</div>
-}
-
-export { WindowWidthTracker };
+// export { WindowWidthTracker };
 export default LifecycleTesting
